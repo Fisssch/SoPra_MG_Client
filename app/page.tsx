b0923ab2f8f764238e380a133f10f6b1ed171fb9
Helinton <helinton.philippathmarajah@uzh.ch>
2025-04-23 15:35:22 +0200

    visual adjustments to the cover page

--- app/page.tsx
+++ app/page.tsx
@@ -37,10 +37,42 @@ export default function Home() {
 	};
 
 	return (
-		<div className="min-h-screen bg-[#a34d3f] flex flex-col items-center text-white text-center px-4 py-12">
-		  <h1 className="text-8xl font-bold mt-50!">Codenames +</h1>
-		  <p className="text-2xl mt-10!">Please register or log in first</p>
-	  
+		<div
+			className="h-screen flex flex-col items-center justify-center text-white text-center px-4"
+			style={{
+				background: 'linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)'
+			}}
+		>
+
+			<h1
+				className="text-8xl font-bold mt-[50px] text-center mb-5!"
+				style={{
+					color: 'white',
+					WebkitTextStroke: '2px transparent',
+					background: 'linear-gradient(to right, #00b4d8, #ff1e00)',
+					WebkitBackgroundClip: 'text',
+					backgroundClip: 'text',
+					WebkitTextFillColor: 'white',
+				}}
+			>
+				Codenames+
+			</h1>
+
+			<h1
+				className="text-xl font-medium text-center tracking-tight"
+				style={{
+					color: 'white',
+					WebkitTextStroke: '1px transparent',
+					background: 'linear-gradient(to right, #00b4d8, #ff1e00)',
+					WebkitBackgroundClip: 'text',
+					backgroundClip: 'text',
+					WebkitTextFillColor: 'white',
+				}}
+			>
+				Please register or log in first
+			</h1>
+
+
 		  {/* First row: Register / Sign in */}
 		  <div className="flex gap-4 mt-8!">
 			<Button type="primary" size="large" onClick={() => router.push("/register")}>
@@ -50,10 +82,10 @@ export default function Home() {
 			  Sign in
 			</Button>
 		  </div>
-	  
+
 		  {/* Spacer row */}
 		  <div className="h-10" />
-	  
+
 		  {/* Second row: Other buttons */}
 		  <div className="flex flex-wrap justify-center gap-4">
 			<Button
