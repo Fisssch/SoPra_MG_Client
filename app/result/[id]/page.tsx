8b5485506f6b1d682996a020e3739b5f73e839fc
Helinton <helinton.philippathmarajah@uzh.ch>
2025-04-23 15:46:04 +0200

    visual adjustments to the result page.

--- app/result/[id]/page.tsx
+++ app/result/[id]/page.tsx
@@ -37,9 +37,7 @@ export default function Result() {
         const lobbyId = localStorage.getItem("lobbyId")?.replace(/^"|"$/g, "");
         const playerId = localStorage.getItem("id")?.replace(/^"|"$/g, "");
 
-        console.log("HELLO1")
         if (token && lobbyId && playerId) {
-            console.log("HELLO2");
             try {
                 await apiService.delete(`/lobby/${lobbyId}/${playerId}`, {
                     Authorization: `Bearer ${token}`,
@@ -70,16 +68,30 @@ export default function Result() {
     const handleBackToLobby = () => {
         const lobbyId = localStorage.getItem("lobbyId")?.replace(/^"|"$/g, "");
         if (lobbyId) {
-            router.push(`/lobby/${lobbyId}`);
+            window.location.href = `/lobby/${lobbyId}`;
         } else {
             alert("Lobby not found.");
         }
     };
 
     return (
-        <div className="min-h-screen bg-[#a34d3f] text-white relative flex flex-col items-center px-4 pt-16">
-
-            <h1 className="text-8xl font-extrabold mt-32! mb-32!">
+        <div
+            className="h-screen flex flex-col items-center justify-center text-white text-center px-4"
+            style={{
+                background: 'linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)'
+            }}
+        >
+            <h1
+                className="text-8xl font-extrabold mt-32! mb-32!"
+                style={{
+                    color: 'white',
+                    WebkitTextStroke: '2px transparent',
+                    background: 'linear-gradient(to right, #00b4d8, #ff1e00)',
+                    WebkitBackgroundClip: 'text',
+                    backgroundClip: 'text',
+                    WebkitTextFillColor: 'white',
+                }}
+            >
                 Team {winningTeam ? winningTeam.charAt(0).toUpperCase() + winningTeam.slice(1).toLowerCase() : ""} has won!
             </h1>
 
