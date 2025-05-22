74d1407b9c59473a5a60c4882d18c07868cdd865
Silvan Wyss <silvansascha.wyss@uzh.ch>
2025-05-22 19:19:49 +0200

    Added better response messages for login & register & slightly changed users overview page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,152 +1,134 @@
-"use client";
-export const dynamic = "force-dynamic";
+'use client';
+export const dynamic = 'force-dynamic';
 
-import "@ant-design/v5-patch-for-react-19";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useApi } from "@/hooks/useApi";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import { User } from "@/types/user";
-import { Button, Form, Input, App } from "antd";
-import { calculateHash } from "@/utils/hash";
-import { EyeOutlined, EyeInvisibleOutlined } from "@ant-design/icons";
+import '@ant-design/v5-patch-for-react-19';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useApi } from '@/hooks/useApi';
+import useLocalStorage from '@/hooks/useLocalStorage';
+import { User } from '@/types/user';
+import { Button, Form, Input, App } from 'antd';
+import { calculateHash } from '@/utils/hash';
+import { EyeOutlined, EyeInvisibleOutlined } from '@ant-design/icons';
 
 interface UserDTO {
-  username: string;
-  password: string;
+	username: string;
+	password: string;
 }
 
 const Login: React.FC = () => {
-  const router = useRouter();
-  const apiService = useApi();
-  const [form] = Form.useForm();
-  const [loading, setLoading] = useState(false);
-  const [initialMessage, setInitialMessage] = useState<string | null>(null);
+	const router = useRouter();
+	const apiService = useApi();
+	const [form] = Form.useForm();
+	const [loading, setLoading] = useState(false);
+	const [initialMessage, setInitialMessage] = useState<string | null>(null);
 
-  const {set: setToken} = useLocalStorage<string>("token", "");
-  const {set: setUserId} = useLocalStorage<string>("id", "");
+	const { set: setToken } = useLocalStorage<string>('token', '');
+	const { set: setUserId } = useLocalStorage<string>('id', '');
 
-  const { message } = App.useApp();
+	const { message } = App.useApp();
 
+	useEffect(() => {
+		const params = new URLSearchParams(window.location.search);
+		const msg = params.get('message');
+		if (msg) {
+			setInitialMessage(msg);
+		}
+	}, []);
 
-  useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const msg = params.get("message");
-    if (msg) {
-      setInitialMessage(msg);
-    }
-  }, []);
+	const handleLogin = async (values: UserDTO) => {
+		setLoading(true);
+		try {
+			const hashedPassword = await calculateHash(values.password);
 
-  const handleLogin = async (values: UserDTO) => {
-    setLoading(true);
-    try {
-      const hashedPassword = await calculateHash(values.password);
+			const { data: user, headers } = await apiService.post<User>('/users/login', {
+				username: values.username,
+				password: hashedPassword,
+			});
 
-      const { data: user, headers } = await apiService.post<User>("/users/login", {
-        username: values.username,
-        password: hashedPassword,
-      });
+			const authHeader = headers.get('Authorization');
+			if (authHeader?.startsWith('Bearer ')) {
+				const token = authHeader.split(' ')[1];
+				setToken(token);
+			}
 
-      const authHeader = headers.get("Authorization");
-      if (authHeader?.startsWith("Bearer ")) {
-        const token = authHeader.split(" ")[1];
-        setToken(token);
-      }
+			if (user?.id) {
+				setUserId(String(user.id));
+				message.success('Login successful!');
+				router.push('/mainpage');
+			}
+		} catch (error: any) {
+			if (error?.status === 404) {
+				message.warning('No user found with given username.');
+			} else if (error?.status === 401) {
+				message.warning('Invalid password.');
+			} else {
+				message.error(`Login failed: ${error?.message || 'Unknown error'}`);
+			}
+		} finally {
+			setLoading(false);
+		}
+	};
 
-      if (user?.id) {
-        setUserId(String(user.id));
-        message.success("Login successful!");
-        router.push("/mainpage");
-      }
-    } catch (error: any) {
-      message.error(`Login failed: ${error.message || "Unknown error"}`);
-    } finally {
-      setLoading(false);
-    }
-  };
+	const handleBackToStart = () => {
+		router.push('/');
+	};
 
-  const handleBackToStart = () => {
-    router.push("/");
-  };
+	return (
+		<div
+			className='min-h-screen flex items-center justify-center text-white text-center px-4 py-12'
+			style={{
+				background: 'linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)',
+			}}>
+			<div className='login-container'>
+				{initialMessage && (
+					<div
+						style={{
+							backgroundColor: '#ffefef',
+							padding: '1rem',
+							borderRadius: '4px',
+							marginBottom: '1rem',
+							color: '#b00020',
+						}}>
+						{initialMessage}
+					</div>
+				)}
 
-  return (
-      <div
-          className="min-h-screen flex items-center justify-center text-white text-center px-4 py-12"
-          style={{
-            background: 'linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)'
-          }}
-      >
-        <div className="login-container">
-          {initialMessage && (
-              <div
-                  style={{
-                    backgroundColor: "#ffefef",
-                    padding: "1rem",
-                    borderRadius: "4px",
-                    marginBottom: "1rem",
-                    color: "#b00020",
-                  }}
-              >
-                {initialMessage}
-              </div>
-          )}
+				<h2 className='text-2xl font-semibold text-black mb-1!'>Login to your account</h2>
 
-          <h2 className="text-2xl font-semibold text-black mb-1!">Login to your account</h2>
+				<Form form={form} name='login' size='large' variant='outlined' onFinish={handleLogin} layout='vertical'>
+					<Form.Item name='username' label={<span className='text-black'>Username</span>} rules={[{ required: true, message: 'Please input your username!' }]}>
+						<Input placeholder='Enter username' />
+					</Form.Item>
+					<Form.Item name='password' label={<span className='text-black'>Password</span>} rules={[{ required: true, message: 'Please input your password!' }]}>
+						<Input.Password
+							placeholder='Enter password'
+							className='bg-white text-black'
+							iconRender={visible => (visible ? <EyeOutlined style={{ color: '#ffffff' }} /> : <EyeInvisibleOutlined style={{ color: '#ffffff' }} />)}
+						/>
+					</Form.Item>
 
-          <Form
-              form={form}
-              name="login"
-              size="large"
-              variant="outlined"
-              onFinish={handleLogin}
-              layout="vertical"
-          >
-            <Form.Item
-                name="username"
-                label={<span className="text-black">Username</span>}
-                rules={[{ required: true, message: "Please input your username!" }]}
-            >
-              <Input placeholder="Enter username" />
-            </Form.Item>
-            <Form.Item
-                name="password"
-                label={<span className="text-black">Password</span>}
-                rules={[{ required: true, message: "Please input your password!" }]}
-            >
-              <Input.Password
-                  placeholder="Enter password"
-                  className="bg-white text-black"
-                  iconRender={(visible) =>
-                      visible ? (
-                          <EyeOutlined style={{ color: "#ffffff" }} />
-                      ) : (
-                          <EyeInvisibleOutlined style={{ color: "#ffffff" }} />
-                      )
-                  }
-              />
-            </Form.Item>
+					<Form.Item>
+						<div style={{ display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'center' }}>
+							<Button type='primary' htmlType='submit' loading={loading} style={{ width: '70%' }}>
+								Login
+							</Button>
+							<Button onClick={handleBackToStart} className='back-button' style={{ width: '70%', fontSize: '0.95rem' }}>
+								Back to Start Page
+							</Button>
+						</div>
+					</Form.Item>
+				</Form>
 
-            <Form.Item>
-              <div style={{ display: "flex", flexDirection: "column", gap: "10px", alignItems: "center" }}>
-                <Button type="primary" htmlType="submit" loading={loading} style={{ width: "70%"}}>
-                  Login
-                </Button>
-                <Button onClick={handleBackToStart} className="back-button" style={{ width: "70%", fontSize: "0.95rem",}}>
-                  Back to Start Page
-                </Button>
-              </div>
-            </Form.Item>
-          </Form>
-
-          <div className="text-center mt-2">
-            <p className="text-xs text-black mb-1">You don&apos;t have an account?</p>
-            <a href="/register" className="text-blue-600 hover:underline font-medium text-sm">
-              Sign up here
-            </a>
-          </div>
-        </div>
-      </div>
-  );
+				<div className='text-center mt-2'>
+					<p className='text-xs text-black mb-1'>You don&apos;t have an account?</p>
+					<a href='/register' className='text-blue-600 hover:underline font-medium text-sm'>
+						Sign up here
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,187 +1,167 @@
 // this code is part of S2 to display a list of all registered users
 // clicking on a user in this list will display /app/users/[id]/page.tsx
-"use client"; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
+'use client'; // For components that need React hooks and browser APIs, SSR (server side rendering) has to be disabled. Read more here: https://nextjs.org/docs/pages/building-your-application/rendering/server-side-rendering
 
-import "@ant-design/v5-patch-for-react-19";
-import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { useApi } from "@/hooks/useApi";
-import { User } from "@/types/user";
-import { Button, Card, Table, Badge, Tooltip, Input } from "antd"; // Import Input component from Ant Design
-import type { TableProps } from "antd"; // antd component library allows imports of types
+import '@ant-design/v5-patch-for-react-19';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useApi } from '@/hooks/useApi';
+import { User } from '@/types/user';
+import { Button, Card, Table, Badge, Tooltip, Input } from 'antd'; // Import Input component from Ant Design
+import type { TableProps } from 'antd'; // antd component library allows imports of types
 // Optionally, you can import a CSS module or file for additional styling:
 // import "@/styles/views/Dashboard.scss";
 
 // Columns for the antd table of User objects
-const columns: TableProps<User>["columns"] = [
-  {
-    title: "Username",
-    dataIndex: "username",
-    key: "username",
-  },
-  {
-    title: "Online Status",
-    dataIndex: "onlineStatus",
-    key: "onlineStatus",
-    render: (status: string) => (
-        <Tooltip title={status}>
-          <Badge
-              color={status === "ONLINE" ? "#52c41a" : "#ffffff"}
-              text={status}
-          />
-        </Tooltip>
-    ),
-  },
+const columns: TableProps<User>['columns'] = [
+	{
+		title: 'Username',
+		dataIndex: 'username',
+		key: 'username',
+	},
+	{
+		title: 'Online Status',
+		dataIndex: 'onlineStatus',
+		key: 'onlineStatus',
+		width: '35%',
+		render: (status: string) => (
+			<Tooltip title={status}>
+				<Badge color={status === 'ONLINE' ? '#52c41a' : '#ffffff'} text={status} />
+			</Tooltip>
+		),
+	},
 ];
 
 const Dashboard: React.FC = () => {
-  const router = useRouter();
-  const apiService = useApi();
-  const [users, setUsers] = useState<User[] | null>(null);
-  const [authorized, setAuthorized] = useState<boolean | null>(null);
-  const [searchQuery, setSearchQuery] = useState<string>(""); // State for search query
+	const router = useRouter();
+	const apiService = useApi();
+	const [users, setUsers] = useState<User[] | null>(null);
+	const [authorized, setAuthorized] = useState<boolean | null>(null);
+	const [searchQuery, setSearchQuery] = useState<string>(''); // State for search query
 
-  useEffect(() => {
-    const token = localStorage.getItem("token")?.replace(/^"|"$/g, "");
-    if (!token) {
-      router.replace("/?message=Please login first.");
-    } else {
-      setAuthorized(true);
-    }
-  }, []);
+	useEffect(() => {
+		const token = localStorage.getItem('token')?.replace(/^"|"$/g, '');
+		if (!token) {
+			router.replace('/?message=Please login first.');
+		} else {
+			setAuthorized(true);
+		}
+	}, []);
 
-  const handleLogout = async () => {
-    let token = localStorage.getItem("token");
+	const handleLogout = async () => {
+		let token = localStorage.getItem('token');
 
-    if (!token) {
-      console.error("Missing token");
-      return;
-    }
+		if (!token) {
+			console.error('Missing token');
+			return;
+		}
 
-    token = token.replace(/^"|"$/g, "");
+		token = token.replace(/^"|"$/g, '');
 
-    try {
-      await apiService.post("/users/logout", null, {
-        Authorization: `Bearer ${token}`,
-      });
+		try {
+			await apiService.post('/users/logout', null, {
+				Authorization: `Bearer ${token}`,
+			});
 
-      localStorage.clear();
-      router.push("/");
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
+			localStorage.clear();
+			router.push('/');
+		} catch (error) {
+			console.error('Logout failed:', error);
+		}
+	};
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const rawToken = localStorage.getItem("token");
-        const token = rawToken?.replace(/^"|"$/g, "");
+	useEffect(() => {
+		const fetchUsers = async () => {
+			try {
+				const rawToken = localStorage.getItem('token');
+				const token = rawToken?.replace(/^"|"$/g, '');
 
-        let users: User[] = await apiService.get<User[]>("/users", {
-          Authorization: `Bearer ${token}`,
-        });
+				let users: User[] = await apiService.get<User[]>('/users', {
+					Authorization: `Bearer ${token}`,
+				});
 
-        // Sort users alphabetically by username
-        users = users.sort((a, b) => a.username.localeCompare(b.username));
+				// Sort users alphabetically by username
+				users = users.sort((a, b) => a.username.localeCompare(b.username));
 
-        setUsers(users);
-      } catch (error) {
-        console.error("Error during loading of the users:", error);
-      }
-    };
+				setUsers(users);
+			} catch (error) {
+				console.error('Error during loading of the users:', error);
+			}
+		};
 
-    if (authorized) {
-      fetchUsers();
-    }
-  }, [authorized, apiService]);
+		if (authorized) {
+			fetchUsers();
+		}
+	}, [authorized, apiService]);
 
-  if (authorized === null) {
-    return null;
-  }
+	if (authorized === null) {
+		return null;
+	}
 
-  // Filter users based on the search query
-  const filteredUsers = users?.filter((user) =>
-    user.username.toLowerCase().includes(searchQuery.toLowerCase())
-  );
+	// Filter users based on the search query
+	const filteredUsers = users?.filter(user => user.username.toLowerCase().includes(searchQuery.toLowerCase()));
 
-  return (
-      <div
-          className="min-h-screen flex items-center justify-center text-white text-center px-4 py-12"
-          style={{
-            background:
-                "linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)",
-          }}
-      >
-        <div className="card-container">
-          <Card
-              title="Overview of users"
-              loading={!users}
-              className="dashboard-container"
-              style={{
-                width: "325px",
-                minHeight: "600px",
-                margin: "0 auto",
-              }}
-          >
-            {users && (
-                <>
-              {/* Search Bar */}
-              <Input.Search
-                placeholder="Search for a user"
-                allowClear
-                onChange={(e) => setSearchQuery(e.target.value)} // Update search query
-                style={{ marginBottom: "16px" }}
-              />
+	return (
+		<div
+			className='min-h-screen flex items-center justify-center text-white text-center px-4 py-12'
+			style={{
+				background: 'linear-gradient(to right, #8b0000 0%, #a30000 10%, #c7adc4 50%,#8cc9d7 70%, #367d9f 90%, #1a425a 100%)',
+			}}>
+			<div className='card-container'>
+				<Card
+					title='Overview of users'
+					loading={!users}
+					className='dashboard-container'
+					style={{
+						width: '500px',
+						minHeight: '600px',
+						margin: '0 auto',
+					}}>
+					{users && (
+						<>
+							{/* Search Bar */}
+							<Input.Search
+								placeholder='Search for a user'
+								allowClear
+								onChange={e => setSearchQuery(e.target.value)} // Update search query
+								style={{ marginBottom: '16px' }}
+							/>
 
-              {/* antd Table: pass the columns and filtered data */}
-              <Table<User>
-                columns={columns}
-                dataSource={filteredUsers} // Use filtered users
-                rowKey="id"
-                pagination={{ pageSize: 5 }}
-                locale={{
-                  emptyText: (
-                    <div style={{ color: "white", fontSize: "16px" }}>
-                      No users found.
-                    </div>
-                  ),
-                }}
-                onRow={(row) => ({
-                  onClick: () => router.push(`/users/${row.id}`),
-                  style: { cursor: "pointer" },
-                })}
-              />
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: "10px",
-                  alignItems: "center",
-                  marginTop: "1px",
-                }}
-              >
-                <Button
-                  onClick={handleLogout}
-                  type="primary"
-                  style={{ width: "70%" }}
-                >
-                  Logout
-                </Button>
-                <Button
-                  onClick={() => router.push("/mainpage")}
-                  type="default"
-                  style={{ width: "70%" }}
-                >
-                  Back to Home
-                </Button>
-              </div>
-            </>
-          )}
-          </Card>
-        </div>
-      </div>
-  );
+							{/* antd Table: pass the columns and filtered data */}
+							<Table<User>
+								columns={columns}
+								dataSource={filteredUsers} // Use filtered users
+								rowKey='id'
+								pagination={{ pageSize: 5, showSizeChanger: false }}
+								locale={{
+									emptyText: <div style={{ color: 'white', fontSize: '16px' }}>No users found.</div>,
+								}}
+								onRow={row => ({
+									onClick: () => router.push(`/users/${row.id}`),
+									style: { cursor: 'pointer' },
+								})}
+							/>
+							<div
+								style={{
+									display: 'flex',
+									flexDirection: 'column',
+									gap: '10px',
+									alignItems: 'center',
+									marginTop: '1px',
+								}}>
+								<Button onClick={handleLogout} type='primary' style={{ width: '70%' }}>
+									Logout
+								</Button>
+								<Button onClick={() => router.push('/mainpage')} type='default' style={{ width: '70%' }}>
+									Back to Home
+								</Button>
+							</div>
+						</>
+					)}
+				</Card>
+			</div>
+		</div>
+	);
 };
 
 export default Dashboard;
